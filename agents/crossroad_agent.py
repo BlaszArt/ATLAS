15c3b44669593a76d9bda638b7cc7c0666deb6bd
Artur Błaszczyk <artur.blaszczyk@onwelo.com>
2018-12-02 14:46:59 +0100

    Neighbours as object. Temporary solution for working simulation. To remove after adding sumo

--- agents/crossroad_agent.py
+++ agents/crossroad_agent.py
@@ -8,26 +8,13 @@ from models.directions import Directions
 
 
 class CrossroadAgent(Agent):
-    def __init__(self, manager_jid, *args, **kwargs):
+    def __init__(self, manager_jid, cars_speed, *args, **kwargs):
         super(CrossroadAgent, self).__init__(*args, **kwargs)
         self.manager_jid = manager_jid
         self.crossroad = crossroad.Crossroad()
         self.neighbours = {}
-        self.neighbours_jid = []
-        self.cars_speed = 0
-
-    def init_crossroad(self, neighbours):
-        self.crossroad.topology['NS'] = []
-        self.crossroad.topology['WE'] = []
-        self.neighbours = neighbours
-        for i in range(0, 4):
-            street = Directions(i).name
-            if i % 2:
-                self.crossroad.topology['NS'].append(street)
-            else:
-                self.crossroad.topology['WE'].append(street)
-            self.crossroad.cars[street] = 0
-            self.crossroad.lights[street] = 1 if i % 2 else 0
+        self.neighbours_jid = {}
+        self.cars_speed = cars_speed
 
     def on_subscribe(self, jid):
         if jid == self.manager_jid:
@@ -36,6 +23,10 @@ class CrossroadAgent(Agent):
 
     def __str__(self):
         return "Agent: {}".format(self.jid)
+
+    def start(self, neighbours, auto_register=True):
+        self.neighbours = neighbours
+        super().start()
 
     def setup(self):
         print(f"[{self.jid}] Hello World! I'm agent {self.jid}")
@@ -52,6 +43,9 @@ class CrossroadAgent(Agent):
 
         # Reporting to manager
         self.add_behaviour(SendReportForSubscribers())
-        #self.add_behaviour(get_cars.GetCars(self, self.cars_speed))
-        #self.add_behaviour(report_situation.ReportSituation([self]))
-        #self.add_behaviour(change_lights.ChangeLights(self))
+
+        # Get data from sensors
+        self.add_behaviour(get_cars.GetCars(self, self.cars_speed))
+
+        # Control lights
+        self.add_behaviour(change_lights.ChangeLights(self))
