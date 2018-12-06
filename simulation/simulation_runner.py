1a84de224219cf0bad7707cc3a215535294409e6
lkotlews <l.kotlewski@stud.elka.pw.edu.pl>
2018-12-07 00:31:00 +0100

    Python sumo starting added

---
+++ simulation/simulation_runner.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+import optparse
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+from sumolib import checkBinary
+import traci
+
+
+
+def run():
+    """execute the TraCI control loop"""
+    step = 0
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        step += 1
+    traci.close()
+    sys.stdout.flush()
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+if __name__ == "__main__":
+    options = get_options()
+
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    traci.start([sumoBinary, "-c", "configuration/simulation.cfg"])
+    run()
