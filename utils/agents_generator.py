bb20523e4a37afb14ee6b8a4ed7eb4b5633223e1
marcin-jamroz <m.jamroz18@gmail.com>
2018-12-09 00:23:17 +0100

    Refaktor topologi, wczytywania, itp. Nie działa protokół rozmowy agentów.

---
+++ utils/agents_generator.py
@@ -0,0 +1,23 @@
+import json
+
+from agents.crossroad_agent import CrossroadAgent
+
+
+class AgentsGenerator:
+    @staticmethod
+    def generate_agents(file, manager_jid):
+        agents = []
+        with open(file, "r") as f:
+            topology = json.load(f)
+
+            for agent_jid, value in topology.items():
+                neighbours = value['neighbours']
+                agents.append(
+                    CrossroadAgent(jid=agent_jid, neighbours=neighbours, password='crossroad' + agent_jid.split('@')[0],
+                                   manager_jid=manager_jid, cars_speed=2))
+        return agents
+
+    @staticmethod
+    def start_agents(agents):
+        for agent in agents:
+            agent.start_crossroad()
