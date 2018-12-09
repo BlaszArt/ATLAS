f00ef87a7bd5ff63b3206fdbc92291a6f2554f6f
marcin-jamroz <m.jamroz18@gmail.com>
2018-12-09 16:39:33 +0100

    Dodanie pliku sumo_api.py z wymaganiami co do api sumo

---
+++ simulation/sumo_api.py
@@ -0,0 +1,9 @@
+class SumoApi:
+    def changeLightDuration(self, agent_jid, lane, duration_change):
+        pass
+
+    def getCarsOnLane(self, agent_jid, lane):
+        pass
+
+    def getLightOnLane(self, agent_jid, lane):
+        pass
